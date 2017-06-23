7cbd2dc84dbb57f4931a6936f513d580b140ac80
刘祺 <qil@jumei.com>
2017-06-23 12:57:53 +0800

    update eslint config

--- test/index.js
+++ test/index.js
@@ -1,60 +1,59 @@
-'use strict';
+'use strict'
 /* global describe, it */
 
-var fs = require('fs');
-var assert = require('assert');
+var fs = require('fs')
+var assert = require('assert')
 
-require('../lib/patch');
+require('../lib/patch')
 
+describe('POSIX', function () {
+  it('/etc/profile', function () {
+    return fs.readFile('/etc/profile')
+      .then(function (contents) {
+        assert.ok(contents.toString())
+      })
+  })
 
-describe('POSIX', function() {
-	it('/etc/profile', function() {
-		return fs.readFile('/etc/profile')
-			.then(function(contents) {
-				assert.ok(contents.toString());
-			});
-	});
+  it('/etc/hosts', function () {
+    return fs.readFile('/etc/hosts')
+      .then(function (contents) {
+        assert.ok(contents.toString())
+      })
+  })
+  it('~', function () {
+    return fs.readdir('~')
+      .then(function (files) {
+        assert.ok(Array.isArray(files))
+      })
+  })
+})
 
-	it('/etc/hosts', function() {
-		return fs.readFile('/etc/hosts')
-			.then(function(contents) {
-				assert.ok(contents.toString());
-			});
-	});
-	it('~', function() {
-		return fs.readdir('~')
-			.then(function(files) {
-				assert.ok(Array.isArray(files));
-			});
-	});
-});
-
-describe('function test', function() {
-	it('fs.outputFile', function() {
-		return fs.outputFile('./test/tmp/test.md', 'test')
-			.then(function() {
-				assert.equal(fs.readFileSync('./test/tmp/test.md'), 'test');
-			})
-			.catch(function() {
-				assert.fail(true);
-			});
-	});
-	it('fs.remove', function() {
-		return fs.remove('./test/tmp')
-			.then(function() {
-				assert.ok(true);
-			})
-			.catch(function() {
-				assert.fail(true);
-			});
-	});
-	it('fs.readFile error catch', function() {
-		return fs.readFile('./test/tmp/test.md')
-			.then(function() {
-				assert.fail(true);
-			})
-			.catch(function() {
-				assert.ok(true);
-			});
-	});
-});
+describe('function test', function () {
+  it('fs.outputFile', function () {
+    return fs.outputFile('./test/tmp/test.md', 'test')
+      .then(function () {
+        assert.equal(fs.readFileSync('./test/tmp/test.md'), 'test')
+      })
+      .catch(function () {
+        assert.fail(true)
+      })
+  })
+  it('fs.remove', function () {
+    return fs.remove('./test/tmp')
+      .then(function () {
+        assert.ok(true)
+      })
+      .catch(function () {
+        assert.fail(true)
+      })
+  })
+  it('fs.readFile error catch', function () {
+    return fs.readFile('./test/tmp/test.md')
+      .then(function () {
+        assert.fail(true)
+      })
+      .catch(function () {
+        assert.ok(true)
+      })
+  })
+})
